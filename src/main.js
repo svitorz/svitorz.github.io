e9163610d8e2cb4ff46882013ff615d30a525513
svitorz <vitor.fabio.castro@gmail.com>
2025-02-10 14:45:48 -0300

    change vite config file

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { createApp } from "vue";
+import { createApp } from "vue/dist/vue.esm-bundler.js";
 import "./style.css";
 import App from "./AppLayout.vue";
 import router from "./routes.js";
