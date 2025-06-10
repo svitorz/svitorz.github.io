be8a44b00bd879f50d40018093b3a9a119b3fb39
svitorz <vitor.fabio.castro@gmail.com>
2025-06-10 14:27:03 -0300

    feat: Add 404.html redirect for SPA refresh

--- src/main.js
+++ src/main.js
@@ -6,4 +6,12 @@ import "./style.css";
 
 import App from "./App.vue";
 
+const redirectPath = sessionStorage.getItem("redirect");
+if (redirectPath) {
+  // Limpa o item para que não redirecione novamente sem motivo
+  sessionStorage.removeItem("redirect");
+  // Usa o router para navegar para a rota que o usuário queria originalmente
+  router.push(redirectPath);
+}
+
 createApp(App).use(routes).mount("#app");
