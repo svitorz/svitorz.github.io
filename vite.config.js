e3469e52633d12fde3ef005c2326c30fbc21e3a2
svitorz <vitor.fabio.castro@gmail.com>
2025-06-10 12:02:26 -0300

    Fix: Adjust base path for GitHub Pages custom domain

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
-  base: "/svitorz.github.io/",
+  base: "/",
   resolve: {
     alias: {
       // sempre use path.resolve para evitar problemas de path
